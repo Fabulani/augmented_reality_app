44a56404d7b702eea58b67d21f89303c5abb44fd
Fabiano Manschein <16107182+Fabulani@users.noreply.github.com>
2024-01-23 21:49:11 +0900

    Add command line args to select method

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import cv2
 import numpy as np
+import sys
 from ar_app.objloader_simple import *
-from ar_app.feature.detector import pipeline
 from ar_app.ar_python3_opencv4 import VideoCapture
 
 
 # ----- PARAMETERS
-OBJ_PATH = "./models/fox/fox.obj"
-REFERENCE_IMG_PATH = "./img/hiro.png"
-CAMERA_PARAMETERS = np.array([
+OBJ_PATH = "./models/fox/fox.obj"  # Path to 3D model (.obj file)
+REFERENCE_IMG_PATH = "./img/hiro.png"  # Path to reference image (marker)
+CAMERA_PARAMETERS = np.array([  # Camera intrinsics matrix
     [800, 0, 320], 
     [0, 800, 240], 
     [0, 0, 1]
 ])
 SCALE3D = 1  # Scale of the 3D model
-#! Change this to select which method to use: marker-based, or feature-based
-PIPELINE_FUNC = pipeline
-# TODO: use cmd args to select the pipeline function to import
+
 
 def main():
+    # Check if the correct number of command line arguments is provided
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <marker or feature>")
+        exit()
+
+    # Get the command line argument
+    argument = sys.argv[1]
+
+    # Check if the argument is either 'marker' or 'feature'
+    if argument not in ['marker', 'feature']:
+        print("Invalid argument. Please enter 'marker' or 'feature'.")
+        exit()
+
+    # Use pipeline from selected method
+    if argument == 'marker':
+        from ar_app.marker.detector import pipeline
+    else:
+        from ar_app.feature.detector import pipeline
+
     obj = OBJ(OBJ_PATH, swapyz=True)
     camera_parameters = CAMERA_PARAMETERS
     reference_image = cv2.imread(REFERENCE_IMG_PATH, 0)
@@ -31,10 +48,10 @@ def main():
         try:
             frame = pipeline(reference_image, frame, camera_parameters, obj, SCALE3D)
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Warning: {e}")
 
         cv2.imshow("frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+        if cv2.waitKey(1) & 0xFF == 27:  # Press ESC to quit
             break
 
     cap.release()
